cf2a146dd5db7eef7ac22508539750dcc0455701
akshat302 <akshatguptamac@gmail.com>
2021-06-02 04:17:04 +0530

    Created a Modal for Login

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,29 +1,40 @@
-import react, { Component, useState, useEffect } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 import Header from './Components/HeaderComponent';
 import Post from './Components/PostComponent';
 import db from './firebase';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
 
   const [posts, setPosts] = useState([]);
-    // {
-    //   username:"akshatguptamac", 
-    //   caption:"Yayyay!! It Works",
-    //   imageUrl:"https://wallpaperaccess.com/full/48071.jpg"
-    // },
-    // {
-    //   username:"avr8", 
-    //   caption:"Nature is beautiful",
-    //   imageUrl:"https://media.cntraveler.com/photos/60596b398f4452dac88c59f8/3:2/w_3999,h_2666,c_limit/MtFuji-GettyImages-959111140.jpg"
-    // },
-    // {
-    //   username:"ravicii", 
-    //   caption:"Abstract Art",
-    //   imageUrl:"https://c4.wallpaperflare.com/wallpaper/246/739/689/digital-digital-art-artwork-illustration-abstract-hd-wallpaper-preview.jpg"
-    // }
-
-
+  const [open, setOpen] = useState(false);
 
   // useEffect runs a piece of code based on a specific condition
 
@@ -31,23 +42,39 @@ function App() {
     //this is where the code runs
     db.collection('posts').onSnapshot(snapshot => {
       //every time a new post is added, this code fires.
-      setPosts(snapshot.docs.map(doc => doc.data())); 
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
   },[]);
    
 
   return (
-    <div className="App">  
-        <Header />
-        <h1>This is an Instagram Clone </h1> 
+    <div className="App">
 
-        {
-        posts.map((post) => {
-          return(
-            <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-          )
-          })
-        }
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal.</h2>
+        </div>
+      </Modal>
+      
+      <Header />
+      <h1>This is an Instagram Clone </h1> 
+
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      
+      {
+      posts.map(({id, post}) => {
+        return(
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        )
+        })
+      }
 
     </div>
   );
